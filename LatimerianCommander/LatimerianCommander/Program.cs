f63749c9ad600435e57dab2aa5af7e96e737f56a
earlybeasts <earlybeasts@proton.me>
2024-07-17 00:00:25 +0200

    Initial commit

--- LatimerianCommander/LatimerianCommander/Program.cs
+++ LatimerianCommander/LatimerianCommander/Program.cs
@@ -1,17 +1,33 @@
 ﻿using Avalonia;
 using Avalonia.ReactiveUI;
 using System;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace LatimerianCommander;
 
 sealed class Program
 {
+    /// <summary>
+    /// Dependency injection service provider
+    /// </summary>
+    public static ServiceProvider Di { get; private set; }
+    
     // Initialization code. Don't use any Avalonia, third-party APIs or any
     // SynchronizationContext-reliant code before AppMain is called: things aren't initialized
     // yet and stuff might break.
     [STAThread]
-    public static void Main(string[] args) => BuildAvaloniaApp()
-        .StartWithClassicDesktopLifetime(args);
+    public static void Main(string[] args)
+    {
+        #region DI setup
+        
+        Di = ConfigureServices()
+            .BuildServiceProvider();
+        
+        #endregion
+        
+        BuildAvaloniaApp()
+            .StartWithClassicDesktopLifetime(args);
+    }
 
     // Avalonia configuration, don't remove; also used by visual designer.
     public static AppBuilder BuildAvaloniaApp()
@@ -20,4 +36,14 @@ sealed class Program
             .WithInterFont()
             .LogToTrace()
             .UseReactiveUI();
+    
+    /// <summary>
+    /// DI setup
+    /// </summary>
+    public static IServiceCollection ConfigureServices()
+    {
+        IServiceCollection services = new ServiceCollection();
+        
+        return services;
+    }
 }
