7d397f30602e556e150544353918b36cf90707aa
earlybeasts <earlybeasts@proton.me>
2024-07-16 23:39:24 +0200

    Initial commit

---
+++ LatimerianCommander/LatimerianCommander/App.axaml.cs
@@ -0,0 +1,28 @@
+using Avalonia;
+using Avalonia.Controls.ApplicationLifetimes;
+using Avalonia.Markup.Xaml;
+using LatimerianCommander.ViewModels;
+using LatimerianCommander.Views;
+
+namespace LatimerianCommander;
+
+public partial class App : Application
+{
+    public override void Initialize()
+    {
+        AvaloniaXamlLoader.Load(this);
+    }
+
+    public override void OnFrameworkInitializationCompleted()
+    {
+        if (ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
+        {
+            desktop.MainWindow = new MainWindow
+            {
+                DataContext = new MainWindowViewModel(),
+            };
+        }
+
+        base.OnFrameworkInitializationCompleted();
+    }
+}
---
+++ LatimerianCommander/LatimerianCommander/ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using ReactiveUI;
+
+namespace LatimerianCommander.ViewModels;
+
+public class ViewModelBase : ReactiveObject
+{
+}
